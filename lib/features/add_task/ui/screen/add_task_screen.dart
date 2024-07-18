bc277bfe9b8d9fc75def9ffbcfeaf4baa5214167
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 01:12:06 +0300

    - add task form - make listener If FormIs Filled to enable button go

--- lib/features/add_task/ui/screen/add_task_screen.dart
+++ lib/features/add_task/ui/screen/add_task_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_tic_tac_toe/core/helpers/spacing.dart';
-import 'package:task_tic_tac_toe/core/widgets/app_text_form_field.dart';
-import '../../../../core/widgets/app_elevated_button.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import '../widgets/add_task_form.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({super.key});
@@ -17,16 +16,7 @@ class AddTaskScreen extends StatelessWidget {
         body: Container(
           margin: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
-            children: [
-              verticalSpace(20),
-              const AppTextFormField(labelText: "Number of task"),
-              verticalSpace(20),
-              const AppTextFormField(labelText: "Sequence of task"),
-              verticalSpace(20),
-              const AppElevatedButton(
-                buttonText: "Go",
-              )
-            ],
+            children: [verticalSpace(20), const AddTaskForm()],
           ),
         ),
       ),
