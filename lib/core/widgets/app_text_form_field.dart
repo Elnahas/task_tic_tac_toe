a0ceac0e34dbf993f5bf446bbb716c45571a6504
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 01:00:20 +0300

    - add controller in AppTextFormField

--- lib/core/widgets/app_text_form_field.dart
+++ lib/core/widgets/app_text_form_field.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppTextFormField extends StatelessWidget {
   final String labelText ;
-  const AppTextFormField({super.key, required this.labelText});
+  final TextEditingController? controller ;
+  const AppTextFormField({super.key, required this.labelText, this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller:  controller,
       decoration: InputDecoration(
           labelText: labelText ,
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
