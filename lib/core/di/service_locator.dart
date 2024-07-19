5bd554ccf109912c60eff4c7c949da51982eae45
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 16:23:36 +0300

    - inject TaskRepository into AddTaskCubit

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:task_tic_tac_toe/core/repositories/task_repository.dart';
+import 'package:task_tic_tac_toe/features/add_task/logic/add_task_cubit.dart';
 import 'package:task_tic_tac_toe/features/splash/data/repo/splash_repository.dart';
 import 'package:task_tic_tac_toe/features/splash/logic/splash_cubit.dart';
 
@@ -8,4 +10,8 @@ Future<void> setupGetIt() async {
   //splash
   getIt.registerSingleton<SplashRepository>(SplashRepository());
   getIt.registerLazySingleton<SplashCubit>(() => SplashCubit(getIt()));
+
+  //Add task
+  getIt.registerSingleton<TaskRepository>(TaskRepository());
+  getIt.registerLazySingleton<AddTaskCubit>(() => AddTaskCubit(getIt()));
 }
