0dc2ea19052ac11baed2c895700ce72d6b752ea3
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 14:54:47 +0300

    - wrap splash screen with BlocProvider at AppRouter

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_tic_tac_toe/core/di/service_locator.dart';
 import 'package:task_tic_tac_toe/core/routing/routes.dart';
+import 'package:task_tic_tac_toe/features/splash/logic/splash_cubit.dart';
 
 import '../../features/splash/ui/screen/splash_screen.dart';
+
 class AppRouter {
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.splash:
-        return MaterialPageRoute(builder: (context) => const SplashScreen());
+        return MaterialPageRoute(
+            builder: (context) => BlocProvider(
+                  create: (context) => SplashCubit(getIt()),
+                  child: const SplashScreen(),
+                ));
       default:
         return null;
     }
