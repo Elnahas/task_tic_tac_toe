04a5d671444dedc6237eda4fdc852e2d4a36df5a
Hazem <hazemelnahas2010@gmail.com>
2024-07-20 00:07:58 +0300

    - hide time from tab completed - make click only when on unassigned tab

--- lib/features/task_list/ui/widgets/task_item.dart
+++ lib/features/task_list/ui/widgets/task_item.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/data/enum/task_status.dart';
 import '../../../../core/data/model/task_model.dart';
 import '../../../../core/helpers/date_helper.dart';
 
 class TaskItem extends StatelessWidget {
   final TaskModel taskModel;
   const TaskItem({
-    super.key, required this.taskModel,
+    super.key,
+    required this.taskModel,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin:const EdgeInsets.only(top: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(10)
-      ),
-      child: ListTile(
-        
-        title: Text(taskModel.title),
-        trailing:  Text(DateHelper.formatTimestampToMinutesAndSeconds(taskModel.dueTime.toDate())),
+    return GestureDetector(
+      onTap: () {
+        if(taskModel.status.name == TaskStatus.unassigned.name){
+
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+            color: Colors.grey[200], borderRadius: BorderRadius.circular(10)),
+        child: ListTile(
+          title: Text(taskModel.title),
+          trailing: taskModel.status.name == TaskStatus.completed.name
+              ? null
+              : Text(DateHelper.formatTimestampToMinutesAndSeconds(
+                  taskModel.dueTime.toDate())),
+        ),
       ),
     );
   }
