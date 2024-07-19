62870265ab6222b45fd2938b55dcdbe499056ee4
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 21:11:27 +0300

    - wrap TaskListScreen by BlocProvider

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -5,6 +5,7 @@ import 'package:task_tic_tac_toe/core/routing/routes.dart';
 import 'package:task_tic_tac_toe/features/add_task/logic/add_task_cubit.dart';
 import 'package:task_tic_tac_toe/features/add_task/ui/screen/add_task_screen.dart';
 import 'package:task_tic_tac_toe/features/splash/logic/splash_cubit.dart';
+import 'package:task_tic_tac_toe/features/task_list/logic/task_list_cubit.dart';
 import 'package:task_tic_tac_toe/features/task_list/ui/screen/task_list_screen.dart';
 
 import '../../features/splash/ui/screen/splash_screen.dart';
@@ -25,7 +26,11 @@ class AppRouter {
                   child: const AddTaskScreen(),
                 ));
       case Routes.taskList:
-        return MaterialPageRoute(builder: (context) => const TaskListScreen());
+        return MaterialPageRoute(
+            builder: (context) => BlocProvider(
+                  create: (context) => TaskListCubit(getIt()),
+                  child: const TaskListScreen(),
+                ));
       default:
         return null;
     }
