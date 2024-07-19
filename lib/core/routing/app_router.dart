f45218fda9abd38607590c71b47928755dfee9d9
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 15:14:24 +0300

    - create AppBlocObserver - add BlocConsumer to splash screen - add 'addTask' to AppRouter - init firebase and BlocObserver in main

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_tic_tac_toe/core/di/service_locator.dart';
 import 'package:task_tic_tac_toe/core/routing/routes.dart';
+import 'package:task_tic_tac_toe/features/add_task/ui/screen/add_task_screen.dart';
 import 'package:task_tic_tac_toe/features/splash/logic/splash_cubit.dart';
 
 import '../../features/splash/ui/screen/splash_screen.dart';
@@ -12,9 +13,11 @@ class AppRouter {
       case Routes.splash:
         return MaterialPageRoute(
             builder: (context) => BlocProvider(
-                  create: (context) => SplashCubit(getIt()),
+                  create: (context) => SplashCubit(getIt())..checkUser(),
                   child: const SplashScreen(),
                 ));
+      case Routes.addTask:
+        return MaterialPageRoute(builder: (context) => const AddTaskScreen());
       default:
         return null;
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_tic_tac_toe/app/my_app.dart';
+import 'package:task_tic_tac_toe/core/helpers/app_bloc_observer.dart';
+import 'package:task_tic_tac_toe/firebase_options.dart';
 
 import 'core/di/service_locator.dart';
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async {
+  Bloc.observer = AppBlocObserver();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   setupGetIt();
-  runApp( MyApp(appRouter: AppRouter()));
+
+  runApp(MyApp(appRouter: AppRouter()));
 }
