c51affad1ca2b5a6d20590377d1b70816e9bcb75
Hazem <hazemelnahas2010@gmail.com>
2024-07-19 12:12:47 +0300

    - create SplashRepo

---
+++ lib/features/splash/data/repo/splash_repo.dart
@@ -0,0 +1,28 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SplashRepo {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> checkUser() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  Future<void> signInAnonymously() async {
+    try {
+      
+      await _auth.signInAnonymously();
+      
+    } catch (e) {
+        throw "Failed to sign in anonymously: $e";
+    }
+  }
+
+
+}
