11e1e6d6e4ee2dc025a0ceaf24a4a7213dcef916
Hazem <hazemelnahas2010@gmail.com>
2024-07-20 08:25:24 +0300

    - create fixed TicTacToeBoard ui

--- lib/features/task_list/ui/widgets/tic_tac_toe_board.dart
+++ lib/features/task_list/ui/widgets/tic_tac_toe_board.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_tic_tac_toe/core/data/enum/task_status.dart';
+import 'package:task_tic_tac_toe/core/helpers/spacing.dart';
 import 'package:task_tic_tac_toe/features/task_list/logic/task_list_cubit.dart';
 
 class TicTacToeBoard extends StatefulWidget {
@@ -11,7 +12,6 @@ class TicTacToeBoard extends StatefulWidget {
 }
 
 class _TicTacToeBoardState extends State<TicTacToeBoard> {
-
   late TaskListCubit _cubit;
 
   @override
@@ -22,11 +22,40 @@ class _TicTacToeBoardState extends State<TicTacToeBoard> {
 
   @override
   Widget build(BuildContext context) {
-    return _cubit.selectedStatus == TaskStatus.assigned.name ?  Column(
-      children: [
-        Text("Player X Turn"),
-        Text("Game"),
-      ],
-    ) : const SizedBox.shrink();
+    return _cubit.selectedStatus == TaskStatus.assigned.name
+        ? Column(
+            children: [
+              verticalSpace(20),
+              const Text("Player X Turn"),
+              verticalSpace(10),
+              GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: GridTile(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "X",
+                            style: TextStyle(fontSize: 40),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          )
+        : const SizedBox.shrink();
   }
 }
