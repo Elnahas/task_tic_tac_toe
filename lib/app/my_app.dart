feb3d1baa48c5de4fce426525c3bca0414fb1b77
Hazem <hazemelnahas2010@gmail.com>
2024-07-21 10:13:13 +0300

    - call dialog loading in add task bloc listener

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           onGenerateRoute: appRouter.onGenerateRoute,
-          initialRoute: Routes.taskList,
+          initialRoute: Routes.splash,
         );
       },
     );
